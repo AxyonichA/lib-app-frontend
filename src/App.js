923d07c82745a3a6b3c6398641f145de2e1dc8bd
Alexandr <aksyonov.it@gmail.com>
2024-04-10 21:05:07 +0500

    Form to add posts done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
-import AddPost from './components/AddPost.js'
-import PostsList from './components/PostsList.js'
+
 export default {
-	components: {
-		'AddPost': AddPost,
-		'PostsList': PostsList
-	},
 	template: `
-		<AddPost />
-		<PostsList />
-	`
+	<form>
+		<label>
+			<p>Название поста</p>
+			<input type='text' v-model="postTitle" />               
+		</label>
+		<label>
+			<p>Содержимое</p>
+			<textarea v-model="postBody" />               
+		</label>
+		<div>
+			<button @click="(e) => {
+				e.preventDefault()
+				postAdd()
+			}">Запостить</button>
+			<button @click="(e) => {
+				e.preventDefault()
+				postTitle = ''
+				postBody = ''
+			}">
+				Отмена
+			</button>
+		</div>
+	</form>
+	<ul style="list-style-type: none">
+	<li v-for="post in posts" :key="post.id">
+		<h2>{{post.title}}</h2>
+		<p>{{post.body}}</p>
+		<button @click="postDelete(post.id)">Удалить пост</button>
+	</li>
+</ul>
+	`,
+	data() {
+		return {
+			postTitle: "",
+			postBody: "",
+			posts: []     
+		}
+	},
+	mounted() {
+		this.getPosts()
+	},
+	methods: {
+		getPosts() {
+			fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
+				.then(response => response.json())
+				.then(data => this.posts = data)
+		},
+		postDelete(id) {
+			this.posts = this.posts.filter(p => p.id !== id)
+		},
+		postAdd() {
+			this.posts = [...this.posts, {title: this.postTitle, body: this.postBody}]
+			this.postTitle = ''
+			this.postBody = ''
+		}
+	}
 }
